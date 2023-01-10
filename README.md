5deadd0caace2004e2c7a1f1c57e48626fb3b44a
Joni Ollila <53558796+natlibfi-jonollil@users.noreply.github.com>
2023-01-10 11:43:53 +0200

    Node 18 & maintenance update (#2)
    * maintenance-update
    * github actions
    * licence-and-node-18
    * set secretOrPrivatekey required

--- README.md
+++ README.md
@@ -52,4 +52,4 @@ headers: {
 
 Copyright (c) 2021-2022 **University Of Helsinki (The National Library Of Finland)**
 
-This project's source code is licensed under the terms of **GNU Lesser General Public License Version 3** or any later version.
+This project's source code is licensed under the terms of **MIT** or any later version.
--- src/index.js
+++ src/index.js
@@ -1,38 +1,14 @@
-/**
-*
-* @licstart  The following is the entire license notice for the JavaScript code in this file.
-*
-* Passport authentication strategy for Melinda using JWT tokens
-*
-* Copyright (C) 2018-2020 University Of Helsinki (The National Library Of Finland)
-*
-* This file is part of passport-melinda-jwt-js
-*
-* passport-melinda-jwt-js program is free software: you can redistribute it and/or modify
-* it under the terms of the GNU Lesser General Public License as
-* published by the Free Software Foundation, either version 3 of the
-* License, or (at your option) any later version.
-*
-* passport-melinda-jwt-js is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU Lesser General Public License for more details.
-*
-* You should have received a copy of the GNU Lesser General Public License
-* along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*
-* @licend  The above is the entire license notice
-* for the JavaScript code in this file.
-*
-*/
-
 import {Strategy as JwtStrategy, ExtractJwt} from 'passport-jwt';
-import jwt from 'jsonwebtoken';
+import {sign as jwtSign} from 'jsonwebtoken';
 
 export default class extends JwtStrategy { }
 export const jwtFromRequest = ExtractJwt.fromAuthHeaderWithScheme('melinda');
 
-export function generateJwtToken(payload, {secretOrPrivateKey = '', issuer = '', audience = '', algorithm = 'HS512'}) {
+export function generateJwtToken(payload, {secretOrPrivateKey = false, issuer = '', audience = '', algorithm = 'HS512'}) {
+  if (secretOrPrivateKey === false) {
+    throw new Error('Set secret or private key to passport!');
+  }
+
   // eslint-disable-next-line functional/immutable-data
   payload.name = undefined;
   // eslint-disable-next-line functional/immutable-data
@@ -40,19 +16,7 @@ export function generateJwtToken(payload, {secretOrPrivateKey = '', issuer = '',
   // eslint-disable-next-line functional/immutable-data
   payload.emails = undefined;
   // eslint-disable-next-line functional/immutable-data
-  payload.exp = makeExpDate();
-  return `melinda ${jwt.sign(payload, secretOrPrivateKey, {issuer, audience, algorithm})}`;
-
-  function makeExpDate() {
-    // eslint-disable-next-line no-var
-    var date = new Date();
-    const monday = (1 + 7 - date.getDay()) % 7;
-    // eslint-disable-next-line functional/immutable-data
-    date.setDate(date.getDate() + monday + 7);
-    // eslint-disable-next-line functional/immutable-data
-    date.setHours(0, 0, 0, 0);
-    return date.getTime() / 1000;
-  }
+  return `melinda ${jwtSign(payload, secretOrPrivateKey, {issuer, audience, algorithm, expiresIn: '120h'})}`;
 }
 
 export function verify(decoded, done) {
@@ -61,10 +25,6 @@ export function verify(decoded, done) {
   }
 
   if (decoded.id) {
-    // eslint-disable-next-line functional/immutable-data
-    decoded.aud = undefined;
-    // eslint-disable-next-line functional/immutable-data
-    decoded.iss = undefined;
     return done(null, decoded);
   }
 
